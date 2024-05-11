eec20a213c66fec42e5c160c5fe54eef4e766ded
Vijaykumar23112000 <mathew5359302@gmail.com>
2024-05-11 13:22:48 +0530

    Day-8 : Spring Security/InMemoryUserDetailsManager impl

---
+++ backend/src/main/java/com/magret/securedoc/security/FilterChainConfiguration.java
@@ -0,0 +1,35 @@
+package com.magret.securedoc.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class FilterChainConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var mathew = User.withDefaultPasswordEncoder()
+                .username("mathew")
+                .password("mathew")
+                .roles("USER")
+                .build();
+        var vijay = User.withDefaultPasswordEncoder()
+                .username("vijay")
+                .password("vijay")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(List.of(vijay,mathew));
+    }
+}
